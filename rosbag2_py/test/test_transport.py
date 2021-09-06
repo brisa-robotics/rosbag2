e46415f6fc7cff9cdbbd723ac46cab115a4f126b
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-09-06 18:21:00 -0300

    add test

--- rosbag2_py/test/test_transport.py
+++ rosbag2_py/test/test_transport.py
@@ -14,8 +14,15 @@
 
 import os
 import sys
+import threading
+import datetime
 
+from common import get_rosbag_options  # noqa
+import rclpy
+from rclpy.node import Node
 from rclpy.qos import QoSProfile
+from std_msgs.msg import String
+import time
 
 if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
     # This is needed on Linux when compiling with clang/libc++.
@@ -25,6 +32,20 @@ if os.environ.get('ROSBAG2_PY_TEST_WITH_RTLD_GLOBAL', None) is not None:
     sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
 
 import rosbag2_py  # noqa
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'test', 10)
+        self.i = 0
+        msg = String()
+        msg.data = "Testing mesage"
+        while(self.i < 10):
+            self.publisher_.publish(msg)
+            time.sleep(1)
+            self.i += 1
 
 
 def test_options_qos_conversion():
@@ -40,3 +61,54 @@ def test_options_qos_conversion():
     record_options = rosbag2_py.RecordOptions()
     record_options.topic_qos_profile_overrides = simple_overrides
     assert record_options.topic_qos_profile_overrides == simple_overrides
+
+
+def test_record_cancel():
+    # tmp_path):
+    """
+    Test for sequential writer.
+
+    :return:
+    """
+    # bag_path = str(tmp_path / 'tmp_write_test')
+    bag_path = '/ws/test'
+    storage_options, converter_options = get_rosbag_options(bag_path)
+
+    recorder = rosbag2_py.Recorder()
+
+    record_options = rosbag2_py.RecordOptions()
+    record_options.all = True
+    record_options.is_discovery_disabled = False
+    # record_options.topics
+    record_options.rmw_serialization_format = ""
+    record_options.topic_polling_interval = datetime.timedelta(milliseconds=100)
+    # record_options.regex = ""
+    # record_options.exclude = ""
+    # record_options.node_prefix = ""
+    record_options.compression_mode = 'file'
+    # record_options.compression_format = 'zstd'
+    # record_options.compression_queue_size = 1
+    # record_options.compression_threads = 0
+    # record_options.topic_qos_profile_overrides = ''
+    # record_options.include_hidden_topics = False
+
+    print("hello there")
+    recorder.record(storage_options, record_options)
+    print("hello there")
+    record_thread = threading.Thread(target=recorder.record,args=(storage_options, record_options),daemon=True)
+    print("hello there")
+    record_thread.start()
+    print("hello there")
+    time.sleep(2)
+    print("after record command")
+    rclpy.init()
+    print("init")
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin()
+
+    recorder.cancel()
+
+    print("Canceled")
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+    assert True
